686512c8abe3486ddfc45ac451994dd1ae20c076
sk <sk@bitsandlikes.de>
2023-06-03 08:44:17 +0200

    fix banner, adjust version

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
-import { exportToCsv , importFromCsv } from './import-export';
-
 const chalk = require('chalk');
 import * as figlet from 'figlet';
 const { program } = require('commander');
-//import * as commander from 'commander';
 
-const banner = figlet.textSync(program.description(), { horizontalLayout: 'full'});
-console.log(chalk.white(banner));
+import { exportToCsv , importFromCsv } from './import-export';
+
 
 program
     .version('0.0.5')
-    .description('i18n import-export cli tool')
+    .description('i18n import-export')
+
+const banner = figlet.textSync(program.description(), { horizontalLayout: 'full'});
+console.log(chalk.white(banner));
 
 // TODO: implement
 program
